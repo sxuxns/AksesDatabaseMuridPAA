cc4505b8b8985ba2000c647b9389cd57bb0bb233
sxuxns <adindayecinthya@gmail.com>
2024-03-31 09:02:18 +0700

    first commit

---
+++ Modul2/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modul2.Models;
+
+namespace Modul2.Controllers
+{
+    public class LoginController : Controller
+    {
+        private string __constr;
+        private IConfiguration __config;
+        public LoginController(IConfiguration configuration)
+        {
+            __config = configuration;
+            __constr = configuration.GetConnectionString("WebApiDatabase");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/Login")]
+        public IEnumerable<Login> LoginUser(string namaUser, string password)
+        {
+            LoginContext context = new LoginContext(this.__constr);
+            List<Login> listlogin = context.Authentifikasi(namaUser, password, __config);
+            return (listlogin).ToArray();
+        }
+    }
+}
---
+++ Modul2/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿namespace Modul2.Models
+{
+    public class Login
+    {
+        public int id_murid { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+        public string id_peran { get; set; }
+        public string nama_peran { get; set; }
+        public string token { get; set; }
+    }
+}
